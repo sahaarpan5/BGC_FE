1af71e855dd54e382cbf480464e162357b2f820b
GCLANDROID <104360689+sahaarpan5@users.noreply.github.com>
2025-09-08 14:47:03 +0530

    AppTSX

--- App.tsx
+++ App.tsx
@@ -11,15 +11,15 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/nvaigation/AppNavigator';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
 
